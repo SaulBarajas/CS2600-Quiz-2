b9791e43b6aefb00ea8213115076f7f6fd5ff14c
SaulBarajas <98624770+SaulBarajas@users.noreply.github.com>
2022-03-11 06:29:25 +0000

    Added 4 new functions to comparisons and wrappers

--- employeeTwo.c
+++ employeeTwo.c
@@ -56,6 +56,22 @@ static int compareEmployeeName(const void *targetPtr, PtrToConstEmployee tableVa
 
 } 
 
+static int compareEmployeePhone(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
+
+{ 
+
+    return strcmp((char *) targetPtr, tableValuePtr->phone);//const void *targetPtr ==> typecast as char pointer then pass into strcmp() 
+
+} 
+
+static int compareEmployeeSalary(const void *targetPtr, PtrToConstEmployee tableValuePtr) 
+
+{ 
+
+    return * (double *) targetPtr != tableValuePtr->salary; //const void *targetPtr ==> typecast as int pointer then dereference 
+
+} 
+
  
 
  
@@ -72,10 +88,29 @@ PtrToEmployee searchEmployeeByNumber(PtrToConstEmployee ptr, int size, long numb
 
 } 
 
+
 PtrToEmployee searchEmployeeByName(PtrToConstEmployee ptr, int size, char* name) 
 
 { 
 
     return searchEmployeeTable(ptr, size, name, compareEmployeeName); 
+
+} 
+
+
+PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int size, char* phone) 
+
+{ 
+
+    return searchEmployeeTable(ptr, size, phone, compareEmployeePhone); 
+
+}
+
+
+PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int size, double salary) 
+
+{ 
+
+    return searchEmployeeTable(ptr, size, &salary, compareEmployeeSalary); 
 
 } 
